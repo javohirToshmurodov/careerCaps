ca08d6983c01c1ab3f90494245a55d304617820e
javohirToshmurodov <rustamnuraliyev1@mail.ru>
2022-06-29 05:36:09 +0500

    psixologiyapart-going

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,39 +1,39 @@
-import { ReactComponent as psixolog } from "../assets/images/psixolog.svg";
-import { ReactComponent as muhandis } from "../assets/images/muhandis.svg";
-import { ReactComponent as dasturchi } from "../assets/images/dasturchi.svg";
-import { ReactComponent as veterinar } from "../assets/images/veterinar.svg";
-import { ReactComponent as arxitektor } from "../assets/images/arxitektor.svg";
+import { ReactComponent as Psixolog } from "../assets/images/psixolog.svg";
+import { ReactComponent as Muhandis } from "../assets/images/muhandis.svg";
+import { ReactComponent as Dasturchi } from "../assets/images/dasturchi.svg";
+import { ReactComponent as Veterinar } from "../assets/images/veterinar.svg";
+import { ReactComponent as Arxitektor } from "../assets/images/arxitektor.svg";
 const jobs = [
   {
-    image: arxitektor,
+    image: Arxitektor,
     jobName: "Arxitektor",
     salary: "45,000$ - 131,00$",
     workFrom: "Masofadan ishlash",
     route: "arxitektor",
   },
   {
-    image: psixolog,
+    image: Psixolog,
     jobName: "Psixolog",
     salary: "39,000$ - 270,00$",
     workFrom: "Masofadan ishlash",
     route: "psixolog",
   },
   {
-    image: veterinar,
+    image: Veterinar,
     jobName: "Veterinar",
     salary: "29,000$ - 200,00$",
     workFrom: "Masofadan ishlash",
     route: "veterinar",
   },
   {
-    image: dasturchi,
+    image: Dasturchi,
     jobName: "Dasturchi",
     salary: "60,000$ - 164,00$",
     workFrom: "Masofadan ishlash",
     route: "dasturchi",
   },
   {
-    image: muhandis,
+    image: Muhandis,
     jobName: "3D Muhandis",
     salary: "86,000$ - 135,00$",
     workFrom: "Masofadan ishlash",
