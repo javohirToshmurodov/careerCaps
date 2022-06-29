90de8e9d9cbc4a65f0ae60529d979417e901f96e
javohirToshmurodov <rustamnuraliyev1@mail.ru>
2022-06-29 06:33:14 +0500

    muhandis-ongoing

--- src/pages/Muhandis/index.js
+++ src/pages/Muhandis/index.js
@@ -3,10 +3,17 @@ import muhandis from "../../assets/images/muhandiss.svg";
 import muhandiss from "../../assets/images/muhandis/muhandisss.svg";
 import { OutlineBtn } from "../../styles";
 import CardMaker from "../../components/CardMaker";
-import model from "../../assets/images/muhandis/model.svg"
-import optimallashtirish from "../../assets/images/muhandis/optimallashtirish.svg"
-import texnologiya from "../../assets/images/muhandis/texnologiya.svg"
-import yangiliklar from "../../assets/images/muhandis/yangiliklar.svg"
+import model from "../../assets/images/muhandis/model.svg";
+import optimallashtirish from "../../assets/images/muhandis/optimallashtirish.svg";
+import texnologiya from "../../assets/images/muhandis/texnologiya.svg";
+import yangiliklar from "../../assets/images/muhandis/yangiliklar.svg";
+import check from "../../assets/images/check.svg";
+import chizmachilik from "../../assets/images/muhandis/chizmachilik.svg";
+import dmodel from "../../assets/images/muhandis/3dmodel.svg";
+import simulatsiya from "../../assets/images/muhandis/simulatsiya.svg";
+import JobsList from "../../components/JobsList";
+import JobLayer from "../../components/JobLayer";
+import layerMuhandis from "../../assets/images/muhandis/layerMuhandis.svg";
 export default function Muhandis() {
   return (
     <>
@@ -37,15 +44,75 @@ export default function Muhandis() {
       <section>
         <div className="w-100">
           <div className="row">
-          <CardMaker
+            <CardMaker
               img={model}
               title={"Model yaratish"}
               description={
-                "Yangi dasturlar uchun kod yozish va sinovdan o'tkazish."
+                "Virtual personajlar, turli narsalarning virtual modellarini yaratadi."
+              }
+            />
+            <CardMaker
+              img={optimallashtirish}
+              title={"Optimallashtirish"}
+              description={
+                "Optimallashtirilgan 2D va 3D formatida real vaqt rejimida renderlash algoritmlarini ishlab chiqish."
+              }
+            />
+            <CardMaker
+              img={texnologiya}
+              title={"Texnologiya"}
+              description={
+                "Sun'iy yo'ldosh tasvirlari, 3D relyef va diqqatga sazovor joylar va geografik vektor ma'lumotlari."
+              }
+            />
+            <CardMaker
+              img={yangiliklar}
+              title={"Yangiliklar bilan tanishish"}
+              description={
+                "3D dizayn, suratga olish va modellashtirish vositalari va texnologiyalaridagi so‘nggi ishlanmalardan xabardor bo‘lib borish"
               }
             />
           </div>
         </div>
+      </section>
+      <section>
+        <div className="container px-4 py-5">
+          <h1 className="bigH1 my-5">
+            3D Muhandis bo'lish uchun nimalarga e'tibor qaratish kerak?
+          </h1>
+          <div className="row px-2">
+            <JobsList
+              check={check}
+              img={chizmachilik}
+              title={"Chizmachilik qobiliyati"}
+              description={
+                "Obyektning o'lchamini, shaklini, tashqi ko'rinishini va uning boshqa xususiyatlarini maksimal aniqlik bilan namoyish qilish imkonini beruvchi virtual modellarni shakllantirish jarayoni."
+              }
+            />
+            <JobsList
+              check={check}
+              img={dmodel}
+              title={"3D modellash uchun dasturlar"}
+              description={
+                "Birinchi toifadagi yetakchilar orasida 3D max , Maya, AutoCad, Cinema 4D, Compass 3D, ikkinchisiga esa Blender 3D modellashtirish , Wings3D va Google SketchUp kiradi."
+              }
+            />
+            <JobsList
+              check={check}
+              img={simulatsiya}
+              title={"Simulyatsiya turlari"}
+              description={
+                "Modellashtirish - bu modellarni yaratish uchun geometrik shakllar va chiziqlar bilan turli xil nuqtalar to'plamining kombinatsiyasi"
+              }
+            />
+          </div>
+        </div>
+      </section>
+      <section className="py-5">
+        <JobLayer
+          layer={layerMuhandis}
+          title={"3D muhandislikni qayerdan o'rgansa bo'ladi?"}
+        />
       </section>
     </>
   );
