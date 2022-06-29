86d8cce585bb15845749884093800621ac65985f
javohirToshmurodov <rustamnuraliyev1@mail.ru>
2022-06-29 18:18:26 +0500

    contact-page-finished

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="text-center minHeightFooter DefaultBg d-flex justify-content-center align-items-center flex-column">
+    <footer className="text-center minHeightFooter mt-5 DefaultBg d-flex justify-content-center align-items-center flex-column">
       <p className="text-white mt-3">Copyright © Toshmurodov Javohir 2022</p>
     </footer>
   );
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="DefaultBg minHeight">
+        <div className="container py-5 px-5 text-white">
+          <div className="row justify-content-between align-items-center">
+            <div className="col-lg-6 col-md-6  col-xl-g col-sm-12 col-12 mainHome">
+              <h1 className="title">CareerCaps</h1>
+              <p className="subtitle">Kelajak kasbingizni biz bilan tanlang</p>
+            </div>
+            <div className="col-lg-6 col-md-6 col-xl-g col-sm-12   col-12">
+              <img className="img-fluid" src={""} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
