816021ad741b5aca3ae83ec7eb1316e15a99b682
SHERBEK <mavlonovsherbek43@gmail.com>
2022-07-15 22:48:02 +0500

    new file

--- src/components/Testing/index.js
+++ src/components/Testing/index.js
@@ -2,98 +2,152 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { getAllData, instance } from '../../redux/actions'
 import Progress from '../ProgressBar'
 import QuizJobCard from '../QuizJobCard'
 import TestCard from '../TestCard'
 import TestFooter from '../TestFooter'
 import { QuizJobCardWrapper } from '../../styles'
+
 export default function Testing() {
-   const [allData, setAllData] = useState({})
-   const allQuestions = useSelector((state) => state.allDatas?.allData)
-   const dispatch = useDispatch()
-   const { id } = useParams()
-   const [disabled, setDisabled] = useState(false)
-   const [isTrue, setIstrue] = useState(false)
-   const [message, setMessage] = useState("Kutilmoqda")
-   const [allQ, setAllQ] = useState({})
-   const [loader, setLoader] = useState(false)
-   const getTestQuestions = async () => {
+  const [allData, setAllData] = useState({})
+  const allQuestions = useSelector((state) => state.allDatas?.allData)
+  const dispatch = useDispatch()
+  const { id } = useParams()
+  const [disabled, setDisabled] = useState(false)
+  const [isTrue, setIstrue] = useState('')
+  const [message, setMessage] = useState('Kutilmoqda')
+  const [allQ, setAllQ] = useState({})
+  const [loader, setLoader] = useState(false)
+  const [check, setCheck] = useState(false)
+  const [showResult, setShowResult] = useState(false)
+  const getTestQuestions = async () => {
+    setCheck(false)
+    try {
+      setLoader(true)
+      const res = await instance.get(
+        `api/v1/platform_user/get_user_question/${id}`
+      )
+      setAllQ(res.data.data)
+      setDisabled(false)
+      setLoader(false)
+      console.log(res.data.data)
+    } catch (err) {
+      console.log('getQuestions err', err)
+    }
+  }
 
-      try {
-         setLoader(true)
-         const response = await instance.get(`api/v1/platform_user/get_user_question/${id}`)
-         setAllQ({ ...response.data.data })
-         console.log("allQ", allQ);
-         console.log("response", response.data.data);
-         setDisabled(false)
-         setLoader(false)
-      } catch (err) {
+  useEffect(() => {
+    getTestQuestions()
+    console.log(allQ)
+  }, [])
 
-      }
-   }
-   useEffect(() => {
-      getTestQuestions()
-      // console.log("hamasi", allQuestions);
-   }, [id])
+  const select = (i) => {
+    setCheck(true)
+    setDisabled(true)
+    const exactAnswer = allQ?.question.answers.filter((item) => item.id === i)
 
-   useEffect(() => {
-      console.log(allQ);
-   }, [allQ])
+    if (exactAnswer[0].isTrue) {
+      setMessage("Javobingiz to'gri")
+      setIstrue(true)
+    } else {
+      setMessage('Javobingiz xato')
+      setIstrue(false)
+    }
+    instance.post('api/v1/platform_user/save_user_answer', {
+      userId: `${allQ.userId}`,
+      answerId: `${exactAnswer[0].id}`,
+    })
+  }
 
-   const select = (i) => {
-      setDisabled(true)
-      const exactAnswer = allQ?.question.answers.filter((item) => item.id === i)
-      console.log(exactAnswer);
-      if (exactAnswer[0].isTrue) {
-         setMessage("Javobingiz to'gri")
-         setIstrue(true)
-      } else {
-         setMessage("Javobingiz xato")
-         setIstrue(false)
-      }
-      instance.post("api/v1/platform_user/save_user_answer", {
-         "userId": `${allQ.userId}`,
-         "answerId": `${exactAnswer[0].id}`
-      }).then((res) => {
-         console.log(res.data);
-      }).catch((err) => {
-         console.log(err);
-      })
-   }
-   if (loader) {
-      return <h1>Loading...</h1>
-   }
-   return (
-      <>
-         <section className='py-5'>
-            <div className="container">
-
-               {/* <Progress now={allQ.questionNumber} />
-
-               <div className="row">
-                  <div className="colorH1">{allQ.questionNumber}. {allQ.question.title}</div>
-               </div>
-               <div className="row justify-content-center">
-                  {
-                     allQ.question.answers.map((e, i) => {
-                        return <label htmlFor={e.id} className='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 mt-3'>
-
-                           <QuizJobCardWrapper>
-                              <div className="positionInput">
-                                 <input defaultChecked={false} disabled={disabled} id={e.id} onChange={() => select(e.id)} name="quizId" type="radio" className="form-check-input" />
-                              </div>
-                              <div>
-                                 <p>{e.answer}</p>
-                              </div>
-                           </QuizJobCardWrapper>
-                        </label>
-                     })}
-               </div> */}
+  if (loader) {
+    return <h1>Loading...</h1>
+  }
+  return (
+    <>
+      {allQ.allQuestionsCount == allQ.questionNumber ? (
+        <div>
+          <button onClick={() => setShowResult(!showResult)}>
+            Natijani korish
+          </button>
+          {showResult && (
+            <div>
+              {allQ.result ? (
+                <p>
+                  {allQ.result.answersCount} ta savoldan
+                  {allQ.result.trueAnswersCount} tasiga to'g'ri javob berdingiz
+                  Test uchun ketkazgan umumiy vaqtingiz : {allQ.result.time}
+                </p>
+              ) : (
+                <div>
+                  <h1>Nomalum xatolik</h1>
+                  <Link to={'/'}>Bosh sahifaga qaytish</Link>
+                </div>
+              )}
             </div>
-         </section>
-         <TestFooter getTestQuestions={getTestQuestions} isTrue={isTrue} message={message} allCount={allQuestions.allQuestionsCount} count={allQuestions.questionNumber} />
-      </>
-   )
+          )}
+        </div>
+      ) : (
+        <>
+          <section className='py-5'>
+            <div className='container'>
+              <Progress now={allQ?.questionNumber} />
+
+              <div className='row'>
+                <div className='colorH1'>
+                  {allQ.question &&
+                    `${allQ?.questionNumber + '' + '.' + allQ.question?.title}`}
+                </div>
+              </div>
+              <div className='row justify-content-center'>
+                {allQ.question ? (
+                  allQ.question?.answers?.map((e, i) => {
+                    return (
+                      <label
+                        key={i}
+                        htmlFor={e.id}
+                        className='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 mt-3'
+                      >
+                        <QuizJobCardWrapper>
+                          <div className='positionInput'>
+                            <input
+                              defaultChecked={false}
+                              disabled={disabled}
+                              id={e.id}
+                              onChange={() => select(e.id)}
+                              name='quizId'
+                              type='radio'
+                              className='form-check-input'
+                            />
+                          </div>
+                          <div>
+                            <p>{e.answer}</p>
+                          </div>
+                        </QuizJobCardWrapper>
+                      </label>
+                    )
+                  })
+                ) : (
+                  <h1>Savollar mavjud emas</h1>
+                )}
+              </div>
+            </div>
+          </section>
+
+          {check ? (
+            <TestFooter
+              getTestQuestions={getTestQuestions}
+              isTrue={isTrue}
+              message={message}
+              allCount={allQ.allQuestionsCount}
+              count={allQ.questionNumber}
+            />
+          ) : (
+            ''
+          )}
+        </>
+      )}
+    </>
+  )
 }
