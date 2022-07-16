67f3905749e9458c115985f3523d93caa733ad53
Javohir Toshmurodov <87381120+javohirToshmurodov@users.noreply.github.com>
2022-07-16 10:24:38 +0500

    javohir

--- src/pages/Reyting/index.js
+++ src/pages/Reyting/index.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import reyting from "../../assets/images/reyting.svg"
+import { instance } from '../../redux/actions'
 export default function Reyting() {
+   const [topUsers, setTopUsers] = useState([])
+   const getTopUsers = () => {
+      instance.get("api/v1/platform_user/top_users?top=10").then((res) => {
+         console.log(res.data.data);
+         setTopUsers(res.data.data)
+      }).catch((err) => {
+         console.log(err);
+      })
+   }
+   useEffect(() => {
+      getTopUsers()
+      console.log("topuserlar", topUsers);
+   }, [])
    return (
-      <div>Reyting</div>
+      <div className='row'>
+         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            {
+               topUsers.map((e, i) => <div className='d-flex justify-content-between'>
+                  <p>
+                     {e.name}
+                  </p>
+                  <p>
+
+                  </p>
+               </div>)
+            }
+         </div>
+         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+
+         </div>
+      </div>
    )
 }
