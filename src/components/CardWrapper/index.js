bf0fa0a64662ea5e394a262933634a9823daf172
abduroshyd <84971351+abduroshyd@users.noreply.github.com>
2022-07-25 08:42:55 +0500

    2

--- src/components/CardWrapper/index.js
+++ src/components/CardWrapper/index.js
@@ -3,7 +3,7 @@ import { MainCardWrapper } from "../../styles";
 import {BASE_URL} from "../../utils/constans";
 export default function CardWrapper(props) {
   return (
-    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 mt-2">
+    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 mt-2 ">
       <MainCardWrapper>
         <div className="crownPosition d-flex justify-content-center align-items-center">
           <img className="img-fluid" src={props.crown} alt="" />
