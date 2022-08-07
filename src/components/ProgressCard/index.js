1eb603f3bd4abeb986cb7fba1b8ca43ca52099d1
Javohir Toshmurodov <87381120+javohirToshmurodov@users.noreply.github.com>
2022-08-07 05:50:43 +0500

    javohir

--- src/components/ProgressCard/index.js
+++ src/components/ProgressCard/index.js
@@ -4,10 +4,10 @@ import { Progress } from 'antd';
 
 export default function ProgressCard(props) {
   return (
-    <div className="col-xl-4 col-lg-4 col-md-4 col-col-sm-6 col-12 ">
+    <div className="col-xl-4 col-lg-4 col-md-6 col-col-sm-6 col-12 ">
       <ProgressCardWrapper className="pt-3">
         <div className="d-flex gap-2">
-          <Progress trailColor="#f1f1f1" format={() => ''} strokeWidth={13} strokeLinecap="butt" type="dashboard" percent={props.soni} />
+          <Progress gapDegree={140} trailColor="#f1f1f1" format={() => ''} strokeWidth={11} type="dashboard" percent={props.soni} />
         </div>
         <div className="text-center position-absolute top-50 my-4">
           <p className="mb-2">{props.title}</p>
